5cb3a4ee93ca74bf43fb6ca072771a61a47217a1
rajeshchoudhury <mchoudhury8083@gmail.com>
2022-08-20 09:43:13 -0400

    my api

--- test.py
+++ test.py
@@ -45,6 +45,38 @@ def fetch_dress_data_mangodb():
     return ("Please check your data and load again")
 
 
+@app.route('/update_dress_mongodb', methods=['GET','POST'])
+def update_dress_data_mangodb():
+    print("START loading data ")
+    try:
+        json_data = request.get_json()
+        print(json_data)
+        dress_table = mongodb['dress_set']
+        Dress_ID = request.json['Dress_ID']
+        dress_table.replace_one({'Dress_ID': Dress_ID}, json_data)
+        return ("Succcessfully Data Loaded")
+    except Exception as e:
+        print(e)
+        return ("System is under maintanance")
+    return ("Please check your data and load again")
+
+@app.route('/delete_dress_mongodb', methods=['GET','POST'])
+def delete_dress_data_mangodb():
+    print("START Deleting data ")
+    try:
+        json_data = request.get_json()
+        print(json_data)
+        dress_table = mongodb['dress_set']
+        Dress_ID = request.json['Dress_ID']
+        dress_table.delete_one({'Dress_ID': Dress_ID})
+        return ("Succcessfully Data Loaded")
+    except Exception as e:
+        print(e)
+        return ("System is under maintanance")
+    return ("Please check your data and load again")
+
+
+
 @app.route('/load_dress', methods=['GET','POST'])
 def load_dress_data():
     print("START loading data ")
